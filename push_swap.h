6b2df0a2bea966437170eccb6da60a010bbc35c6
Meline <melineharutyunyan168@gmail.com>
2026-03-30 13:11:53 +0400

    .h change

--- push_swap.h
+++ push_swap.h
@@ -26,5 +26,16 @@ int check_one_arg (char *ptr);
 t_list	*arr_int_to_lst(char *p);
 int *parse_args(char **arr, int *size);
 t_list *arr_to_list(int *arr, int size);
+void    sa(t_stack **a);
+void    sb(t_stack **b);
+void    ss(t_stack **a, t_stack **b);
+void    pa(t_stack **a, t_stack **b);
+void    pb(t_stack **a, t_stack **b);
+void    ra(t_stack **a);
+void    rb(t_stack **b);
+void    rr(t_stack **a, t_stack **b);
+void    rra(t_stack **a);
+void    rrb(t_stack **b);
+void    rrr(t_stack **a, t_stack **b);
 
 #endif
